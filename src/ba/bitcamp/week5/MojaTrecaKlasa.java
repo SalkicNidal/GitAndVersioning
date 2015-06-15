9a246f2a5bf742be7221d62c2038ff3727a093ce
Nidal Salkic <nidal.salkic@BitCamps-Mac-mini-94.local>
2015-06-15 11:06:46 +0200

    Change one

--- src/ba/bitcamp/week5/MojaTrecaKlasa.java
+++ src/ba/bitcamp/week5/MojaTrecaKlasa.java
@@ -1,5 +1,79 @@
 package ba.bitcamp.week5;
 
 public class MojaTrecaKlasa {
+	private String firstName;
+	private String lastName;
+	private String country;
+	private String  city;
+	private String phoneNumber;
+	private Integer age;
+	
+	
+	public MojaTrecaKlasa() {
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
+	
 
 }
